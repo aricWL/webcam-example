d106be62a7cf30017ed266dee851aefc2701c4c7
Matt Lane <lane.matthew@gmail.com>
2017-03-17 09:20:50 -0700

    adding s3 integration

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+import boto3
+import os
 
 app = Flask(__name__)
 
@@ -10,7 +12,15 @@ def index():
 def video():
   filename = request.form['fname']
   file = request.files['data']
-  file.save('./' + filename)
+  s3 = boto3.resource('s3',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+  )
+  s3.Bucket('cams-videos').put_object(
+    Key=filename, 
+    Body=file, 
+    ServerSideEncryption='AES256'
+  )
   return jsonify({"message": "complete!"})
 
 if __name__ == "__main__":
